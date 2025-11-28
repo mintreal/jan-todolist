c183a30acf4a63b96026d2bb5461988108b99d30
Jeong <lindento@gmail.com>
2025-11-28 15:32:55 +0900

    로그인 텍스트 변경

--- frontend/src/components/Auth/LoginForm.jsx
+++ frontend/src/components/Auth/LoginForm.jsx
@@ -32,7 +32,7 @@ function LoginForm() {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-bold text-gray-900">
-            로그인 테스트
+            엔꼼 로그인
           </h2>
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
